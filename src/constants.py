d6660218993030eb6a248babcd2ac1107d89d989
pannet1 <prog@ecomsense.in>
2025-02-26 02:24:46 +0530

    fix  path

--- src/constants.py
+++ src/constants.py
@@ -12,7 +12,7 @@ from toolkit.logger import Logger
 from toolkit.fileutils import Fileutils
 
 O_FUTL = Fileutils()
-S_DATA = "data/"
+S_DATA = "../data/"
 S_LOG = S_DATA + "log.txt"
 
 if not O_FUTL.is_file_exists(S_LOG):
@@ -40,16 +40,16 @@ def yml_to_obj(arg=None):
         # return the parent folder name
         parent = path.dirname(path.abspath(__file__))
         print(f"{parent=}")
-        """
         grand_parent_path = path.dirname(parent)
         print(f"{grand_parent_path=}")
         folder = path.basename(grand_parent_path)
         """
         folder = path.basename(parent)
+        """
         # reverse the words seperated by -
         lst = folder.split("-")
         file = "_".join(reversed(lst))
-        file = "../" + file + ".yml"
+        file = "../../" + file + ".yml"
     else:
         file = S_DATA + arg
 
